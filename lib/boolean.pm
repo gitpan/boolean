8517512e6eb49ec893c30cbc67c217630da498a4
Ingy döt Net <ingy@cpan.org>
2008-08-25 02:52:07 +0000

    Import of INGY/boolean-0.20 from CPAN.
    gitpan-cpan-distribution: boolean
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         INGY/boolean-0.20.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/boolean.pm
+++ lib/boolean.pm
@@ -1,20 +1,54 @@
 package boolean;
 use 5.005003;
 use strict;
-$boolean::VERSION = '0.12';
+# use warnings;
+$boolean::VERSION = '0.20';
 
-use XSLoader;
-XSLoader::load 'boolean';
+use overload
+    '""' => sub {
+        ${$_[0]};
+    },
+#     '${}' => sub {
+#         require Carp;
+#         Carp::croak("Attempt to dereference boolean value is illegal");
+#     },
+    fallback => 1;
 
 use base 'Exporter';
-
-@boolean::EXPORT_OK = qw(
-    true false isTrue isFalse isBoolean
-    null isNull
-);
+@boolean::EXPORT = qw(true false);
+@boolean::EXPORT_OK = qw(isTrue isFalse isBoolean);
 %boolean::EXPORT_TAGS = (
-    all => [qw(true false isTrue isFalse isBoolean)],
+    all    => [@boolean::EXPORT, @boolean::EXPORT_OK],
+    test   => [qw(isTrue isFalse isBoolean)],
 );
+
+my ($true, $false);
+my ($true_val, $false_val, $bool_vals);
+
+BEGIN {
+    $true  = do {bless \(my $t = 1), 'boolean'};
+    $false = do {bless \(my $f = 0), 'boolean'};
+    $true_val  = overload::StrVal($true);
+    $false_val = overload::StrVal($false);
+    $bool_vals = {$true_val => 1, $false_val => 1};
+}
+
+sub true()  { $true }
+sub false() { $false }
+sub isBoolean {
+    not(defined $_[0]) ? false :
+    (exists $bool_vals->{overload::StrVal($_[0])}) ? true : false;
+}
+sub isTrue  {
+    not(defined $_[0]) ? false :
+    (overload::StrVal($_[0]) eq $true_val)  ? true : false;
+}
+sub isFalse {
+    not(defined $_[0]) ? false :
+    (overload::StrVal($_[0]) eq $false_val) ? true : false;
+}
+
+1;
 
 =encoding utf8
 
@@ -24,10 +58,14 @@ boolean - Boolean support for Perl
 
 =head1 SYNOPSIS
 
-    use boolean ':all';
+    use boolean;
 
     do &always if true;
     do &never if false;
+
+and:
+
+    use boolean ':all';
 
     $guess = int(rand(2)) % 2 ? true : false;
 
@@ -50,133 +88,83 @@ values are false:
 
 Every other scalar value is true.
 
-A lesser known aspect of Perl is that its C API has two constant scalar
-values:
+This module provides basic Boolean support, by defining two special
+objects: C<true> and C<false>.
+
+=head1 IMPLEMENTATION NOTE
+
+Version 0.20 is a complete rewrite from version 0.12. The old version
+used XS and had some fundamental flaws. The new version is pure Perl and
+is more correct. The new version depends on overload.pm to make the
+true and false objects return 1 and 0 respectively.
+
+The "null" support found in 0.12 was also removed as superfluous.
+
+=head1 RATIONALE
+
+When sharing data between programming languages, it is important to
+support the same group of basic types. In Perlish programming languages,
+these types include: Hash, Array, String, Number, Null and Boolean. Perl
+lacks native Boolean support.
+
+Data interchange modules like YAML and JSON can now C<use boolean> to
+encode/decode/roundtrip Boolean values.
+
+=head1 FUNCTIONS
+
+This module defines the following functions:
 
 =over
 
-=item C<PL_sv_yes>
+=item true
 
-This is the "true" SV. It is a Perl internals constant scalar with a
-value of 1. See L<perlapi> for more info.
+This function returns a scalar value which should evaluate to true. The
+value is a singleton object, meaning there is only one "true" value in a
+Perl process at any time. You can check to see whether the value is the
+"true" object with the isTrue function described below.
 
-=item C<PL_sv_no>
+=item false
 
-This is the "false" SV. It is a Perl internals constant scalar with a
-value of ''. See L<perlapi> for more info.
+This function returns a scalar value which should evaluate to false. The
+value is a singleton object, meaning there is only one "false" value in a
+Perl process at any time. You can check to see whether the value is the
+"false" object with the isFalse function described below.
+
+=item isTrue($scalar)
+
+Returns C<boolean::true> if the scalar passed to it is the
+C<boolean::true> object. Returns C<boolean::false> otherwise.
+
+=item isFalse($scalar)
+
+Returns C<boolean::true> if the scalar passed to it is the
+C<boolean::false> object. Returns C<boolean::false> otherwise.
+
+=item isBoolean($scalar)
+
+Returns C<boolean::true> if the scalar passed to it is the
+C<boolean::true> or C<boolean::false> object. Returns C<boolean::false>
+otherwise.
 
 =back
 
-The C<boolean> module exposes these obscure values as the subroutines
-C<true> and C<false>. Finding meaningful ways to use these in a Perl
-program is left as an exercise for the reader.
+=head1 EXPORTABLES
 
-=head1 IMPORTABLE SUBROUTINES
+By default this module exports the C<true> and C<false> functions.
 
-This module does not export any subroutines by default. It exports all
-of the following subroutines if you use the following invocation:
+The module also defines these export tags:
 
-    use boolean ':all';
+=over
 
-=head2 true
+=item :all
 
-    true()
+Exports C<true>, C<false>, C<isTrue>, C<isFalse>, C<isBoolean>
 
-This function always returns a scalar whose value is the Perl internals
-constant C<PL_sv_yes>. The function acts like a constant and thus takes
-no input parameters.
+=item :test
 
-=head2 false
+Exports C<isTrue>, C<isFalse>, C<isBoolean>
 
-    false()
-
-This function always returns a scalar whose value is the Perl internals
-constant C<PL_sv_no>. The function acts like a constant and thus takes
-no input parameters.
-
-=head2 isTrue
-
-    isTrue($scalar)
-
-Returns C<true> if the scalar passed to it is actually C<PL_sv_yes>.
-Returns C<false> otherwise.
-
-=head2 isFalse
-
-    isFalse($scalar)
-
-Returns C<true> if the scalar passed to it is actually C<PL_sv_no>.
-Returns C<false> otherwise.
-
-=head2 isBoolean
-
-    isBoolean($scalar)
-
-Returns C<true> if the scalar passed to it is actually C<PL_sv_yes> or
-C<PL_sv_no>. Returns C<false> otherwise.
-
-=head1 OTHER IMPORTABLE SUBROUTINES
-
-It turns out that the value for C<undef> works out the same as true and
-false in Perl internals. All scalar values set to undef actually point
-to the same scalar value internally, C<PL_sv_undef>.
-
-This module exposes corresponding subroutines for the undef value,
-giving it the name "C<null>" to match common programming language
-terminology.
-
-=head2 null()
-
-This function always returns a scalar whose value is the Perl internals
-constant C<PL_sv_undef>. The function acts like a constant and thus takes
-no input parameters.
-
-=head2 isNull($scalar)
-
-Returns C<true> if the scalar passed to it is actually C<PL_sv_undef>.
-Returns C<false> otherwise.
-
-=head1 YAML AND JSON
-
-It is true by definition that all valid JSON streams are valid YAML
-streams. The YAML and JSON specification writers have worked together to
-ensure this.
-
-This leads to the following implications for Boolean values.
-
-JSON supports 3 data types and null values:
-
-    {
-        "string type": "all strings are double quoted in JSON",
-        "number type": [1, 2, 3.1415],
-        "boolean type": [true, false],
-        "null type": null
-    }
-
-Strings are always double quoted. All hash keys are strings (thus
-quoted). Only numbers (integer and floating point) boolean values and
-null values are unquoted.
-
-I recommend that all Perl YAML and JSON implementations always Dump
-scalar strings with the value "true", "false" or "null", using quotes.
-
-Unquoted values of this set can be Loaded into Perl using the same
-constants as C<boolean.pm> does. When these values are Dumped they are
-unquoted. Pure Perl YAML and JSON implementations can use C<boolean.pm>
-to accomplish this.
-
-=head1 BUGS
-
-Currently, assigning a boolean value to a variable causes the booleanity
-to be lost. This code dies:
-
-    $x = true;
-    isBoolean($x) || isTrue($x) or die;
-
-If you know how to solve this using SV magic or otherwise, please let me
-know. I'm trying to fix it myslf now. Hoping to not need to resort to:
-
-    setTrue($x);
+=back
 
 =head1 AUTHOR
 
